50f7fab3aaeff5c2aa3a643e9322b9cf7746fad5
Orzhov <alouis@stagiaire-humanbooster.com>
2025-06-06 12:31:58 +0200

    feat: Personnage

--- src/personnages/Personnage.java
+++ src/personnages/Personnage.java
@@ -8,22 +8,80 @@ public abstract class Personnage {
     protected int defense;
 
     // Constructeur
+    public Personnage(String nom, int pv, int attaque, int defense) {
+        this.nom = nom;
+        this.pv = pv;
+        this.attaque = attaque;
+        this.defense = defense;
+    }
+
     // getters & setters
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getPv() {
+        return pv;
+    }
+
+    public void setPv(int pv) {
+        this.pv = pv;
+    }
+
+    public int getAttaque() {
+        return attaque;
+    }
+
+    public void setAttaque(int attaque) {
+        this.attaque = attaque;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
 
     // Méthode attaquer: calcule dégâts et appelle cible.prendreDegats()
     public void attaquer(Personnage cible) {
-        // TODO
+        // Calcul des dégâts bruts
+        int degats = this.attaque - cible.getDefense();
+
+        // Dégâts minimum pour éviter le personnage invincible
+        if (degats < 1) {
+            degats = 1;
+        }
+
+        // Appliquer les dégâts à la cible
+        cible.prendreDegats(degats);
+
+        // Afficher un message au joueur
+        System.out.println(this.nom + " attaque " + cible.getNom() + " et inflige " + degats + " points de dégâts !");
     }
 
-    // Réduit les PV selon dégats reçus
-    public void prendreDegats(int degats) {
-        // TODO
+public void prendreDegats(int degats) {
+    // On soustrait les dégâts reçus aux points de vie actuels
+    this.pv -= degats;
+
+    // Si les points de vie descendent en dessous de zéro, on les met à zéro
+    if (this.pv < 0) {
+        this.pv = 0;
     }
+
+    // Affiche le nouvel état des points de vie pour feedback
+    System.out.println(this.nom + " a maintenant " + this.pv + " PV.");
+}
 
     // Retourne vrai si pv > 0
     public boolean estVivant() {
-        // TODO
-        return false;
+// Si les points de vie sont supérieurs à 0, alors il est vivant
+        return this.pv > 0;
     }
 
     // (Optionnel) afficher fiche de stats

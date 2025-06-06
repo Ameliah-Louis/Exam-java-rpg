c9607e88cacfb3995e7f20d808b08b5c0c1fedfd
Orzhov <alouis@stagiaire-humanbooster.com>
2025-06-06 13:19:43 +0200

    feat: Ennemi et Gobelin

--- src/personnages/Ennemi.java
+++ src/personnages/Ennemi.java
@@ -1,10 +1,30 @@
 package personnages;
 
-// Classe abstraite des ennemis
+import java.util.Random;
+
+// Ennemi hérite de Personnage, donc possède nom, pv, attaque, défense, etc.
 public abstract class Ennemi extends Personnage {
-    // Liste de deux attaques spéciales (méthodes ou objets)
 
-    // Constructeur -> initialise stats selon type
+    protected Random random = new Random(); // pour choisir une attaque aléatoirement
 
-    // Méthode attaquer override? Choisit aléatoirement entre attaques de base ou spéciales
+    // Constructeur de base pour un Ennemi, on passe ses stats au parent
+    public Ennemi(String nom, int pvInitial, int attaque, int defense) {
+        super(nom, pvInitial, attaque, defense);
+    }
+
+    // Méthode abstraite : chaque ennemi doit définir ses 2 attaques spécifiques
+    protected abstract void attaque1(Personnage cible);
+    protected abstract void attaque2(Personnage cible);
+
+    // Méthode publique pour attaquer le héros au tour par tour
+    public void attaquer(Personnage cible) {
+        // Choix aléatoire entre attaque1 et attaque2
+        int choix = random.nextInt(2); // 0 ou 1 mais pas booléen pour se laisser la possibilité d'ajouter facilement des attaques
+
+        if (choix == 0) {
+            attaque1(cible);
+        } else {
+            attaque2(cible);
+        }
+    }
 }
---
+++ src/personnages/Gobelin.java
@@ -0,0 +1,27 @@
+package personnages;
+
+public class Gobelin extends Ennemi {
+
+    public Gobelin() {
+        // Appel du constructeur parent avec nom et stats spécifiques au Gobelin
+        super("Gobelin", 50, 10, 5);  // PV=50, Attaque=10, Défense=5 (exemple)
+    }
+
+    // Override Attaque 1 : attaque simple, inflige dégâts basiques
+    @Override
+    protected void attaque1(Personnage cible) {
+        int degats = this.getAttaque() - cible.getDefense();
+        if (degats < 0) degats = 0;
+        System.out.println(getNom() + " frappe " + cible.getNom() + " !");
+        cible.prendreDegats(degats);
+    }
+
+    // Attaque 2 : attaque spéciale un peu plus forte
+    @Override
+    protected void attaque2(Personnage cible) {
+        int degats = this.getAttaque() + 5 - cible.getDefense();  // +5 dégâts bonus
+        if (degats < 0) degats = 0;
+        System.out.println(getNom() + " lance un coup puissant en hurlant sur " + cible.getNom() + " !");
+        cible.prendreDegats(degats);
+    }
+}
